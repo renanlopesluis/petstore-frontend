63e712f4e62b8f08d303f889e178814c87806cae
Renan Lopes <renan.ilegra@terceiros.zenvia.com>
2019-09-05 14:25:51 -0300

    testing pettype.service

--- src/app/service/pet-type/pettype.service.spec.ts
+++ src/app/service/pet-type/pettype.service.spec.ts
@@ -1,12 +1,28 @@
-import { TestBed } from '@angular/core/testing';
-
 import { PetTypeService } from './pettype.service';
+import { of } from 'rxjs'
+import { UtilsService } from '../utils/utils.service';
 
 describe('PetTypeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let utils: UtilsService;
+  let httpClientSpy: { get: jasmine.Spy };
+  let petTypeService: PetTypeService;
+  let types: Array<any>;
+  
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    utils = new UtilsService();
+    petTypeService = new PetTypeService(<any> httpClientSpy, utils);
+    types = [{id: 1, description: "Dog"},{id: 2, description: "Cat"}];
+  });
+  
 
-  it('should be created', () => {
-    const service: PetTypeService = TestBed.get(PetTypeService);
-    expect(service).toBeTruthy();
+  it('should list pet types', () => {
+    httpClientSpy.get.and.returnValue(of(types));
+    petTypeService.list().subscribe(
+      heroes => expect(heroes).toEqual(types, 'types'),
+      fail
+    );
+
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
